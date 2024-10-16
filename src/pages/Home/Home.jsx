56d92a8c2337e9492ea5288f13b17d885ad847af
Majedur Rahman <md.majedur97@gmail.com>
2024-10-16 20:49:28 -0230

    moodHeader-added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,27 +11,29 @@ function Home() {
           </p>
           <p className="home__quoteAuthor">-anonymous</p>
         </section>
-
-        <section className="home__moodSelector">
-          <div className="home__joyful">
-            😊
-            <span className="home__joyfulText">joyful</span>
-          </div>
-          <div className="home__content">
-            🙂
-            <span className="home__contentText">content</span>
-          </div>
-          <div className="home__confused">
-            😕
-            <span className="home__confusedText">confused</span>
-          </div>
-          <div className="home__angry">
-            😠
-            <span className="home__angryText">angry</span>
-          </div>
-          <div className="home__crying">
-            😢
-            <span className="home__cryingText">crying</span>
+        <section className="home__moodContainer">
+          <p className="home__moodHeader">How's you mood right now?</p>
+          <div className="home__moodSelector">
+            <div className="home__joyful">
+              😊
+              <span className="home__joyfulText">joyful</span>
+            </div>
+            <div className="home__content">
+              🙂
+              <span className="home__contentText">content</span>
+            </div>
+            <div className="home__confused">
+              😕
+              <span className="home__confusedText">confused</span>
+            </div>
+            <div className="home__angry">
+              😠
+              <span className="home__angryText">angry</span>
+            </div>
+            <div className="home__crying">
+              😢
+              <span className="home__cryingText">crying</span>
+            </div>
           </div>
         </section>
       </main>
