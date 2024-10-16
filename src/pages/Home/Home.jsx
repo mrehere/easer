6eb5b61c3e2a6d7ef405a7cf35c7e46c65b3cd86
Majedur Rahman <md.majedur97@gmail.com>
2024-10-16 20:13:39 -0230

    card-emojibox-created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,40 @@
 import Header from "../../Components/Header/Header.jsx";
+import "./Home.scss";
 function Home() {
   return (
     <>
-      <Header title="easer" subtitle="welcom to your nest!" />
-      <h1>Hello from Home</h1>
+      <main className="home">
+        <Header title="easer" subtitle="welcom to your nest!" />
+        <section className="home__card">
+          <p className="home__quote">
+            Quote of the day Quote of the day Quote of the day
+          </p>
+          <p className="home__quoteAuthor">-anonymous</p>
+        </section>
+
+        <section className="home__moodSelector">
+          <div className="home__joyful">
+            😊
+            <span className="home__joyfulText">joyful</span>
+          </div>
+          <div className="home__content">
+            🙂
+            <span className="home__contentText">content</span>
+          </div>
+          <div className="home__confused">
+            😕
+            <span className="home__confusedText">confused</span>
+          </div>
+          <div className="home__angry">
+            😠
+            <span className="home__angryText">angry</span>
+          </div>
+          <div className="home__crying">
+            😢
+            <span className="home__cryingText">crying</span>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
