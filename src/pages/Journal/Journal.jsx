bcbc12ff80bf3f776b132c24b3c0d6b9114f6ffb
Majedur Rahman <md.majedur97@gmail.com>
2024-10-19 14:11:43 -0230

    timestampConversion-function-created

--- src/pages/Journal/Journal.jsx
+++ src/pages/Journal/Journal.jsx
@@ -1,10 +1,29 @@
+import "./Journal.scss";
+
 import Header from "../../Components/Header/Header.jsx";
 import Footer from "../../Components/Footer/Footer.jsx";
+import JournalBox from "../../Components/JournalBox/JournalBox.jsx";
 function Journal() {
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Intl.DateTimeFormat("en-us", options).format(date);
+  };
   return (
     <>
-      <Header title="journal" subtitle="Unwind, Reflect, and Heal!" />
-      <Footer />
+      <main className="journal">
+        <Header title="journal" subtitle="Unwind, Reflect, and Heal!" />
+        <div className="journal__dateContainer">
+          <p className="journal__date">{formatDate(Date.now())}</p>
+        </div>
+        <JournalBox />
+        <Footer />
+      </main>
     </>
   );
 }
