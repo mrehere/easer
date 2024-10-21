9c8af27f935ab8d3f9f62779029acc743e47bf56
Majedur Rahman <md.majedur97@gmail.com>
2024-10-21 18:09:50 -0230

    basic-moodCard-implemented

--- src/Components/MoodAnalytics/MoodAnalytics.jsx
+++ src/Components/MoodAnalytics/MoodAnalytics.jsx
@@ -2,7 +2,7 @@ import "./MoodAnalytics.scss";
 import React from "react";
 
 function MoodAnalytics() {
-  return <div>Mood Analytics</div>;
+  return <div>Mood Analytics under development</div>;
 }
 
 export default MoodAnalytics;
--- src/Components/MoodHistory/MoodHistory.jsx
+++ src/Components/MoodHistory/MoodHistory.jsx
@@ -1,11 +1,35 @@
 import "./MoodHistory.scss";
 
-import React from "react";
-
 function MoodHistory() {
+  const moods = [
+    {
+      userId: "12345",
+      moodId: "m2",
+      moodName: "content",
+      createdAt: 1727962226189,
+    },
+    {
+      userId: "12345",
+      moodId: "m1",
+      moodName: "joyful",
+      createdAt: 1729171826189,
+    },
+    {
+      userId: "12345",
+      moodId: "m5",
+      moodName: "crying",
+      createdAt: 1727357426189,
+    },
+  ];
   return (
     <section className="moodHistory">
-      <h1 className="moodHistory__title">Mood History</h1>
+      <h4 className="moodHistory__title">Mood History</h4>
+
+      <div className="moodHistory__container">
+        <p className="moodHistory__date">Logged on: Monday, October 21, 2024</p>
+        <p className="moodHistory__mood">😊</p>
+        <p className="moodHistory__name">Joyful</p>
+      </div>
     </section>
   );
 }
