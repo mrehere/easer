1ee3cf77ed68622dd6d5d38ce5fcfd3bb5fa52f3
Majedur Rahman <md.majedur97@gmail.com>
2024-10-21 15:22:10 -0230

    Implement mood selection and preserve functionality

--- src/Components/MoodBox/MoodBox.jsx
+++ src/Components/MoodBox/MoodBox.jsx
@@ -1,35 +1,109 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./MoodBox.scss";
 
 function MoodBox() {
+  const moodRef = [
+    {
+      moodId: "m1",
+      moodName: "joyful",
+    },
+    {
+      moodId: "m2",
+      moodName: "content",
+    },
+    {
+      moodId: "m3",
+      moodName: "angry",
+    },
+    {
+      moodId: "m4",
+      moodName: "confused",
+    },
+    {
+      moodId: "m5",
+      moodName: "crying",
+    },
+  ];
+
+  const [currentMood, setCurrentMood] = useState("");
+  const [postCurrentMood, setPostCurrentMood] = useState("");
+
+  // -------handling the mood selection-------
+  const handleMoodSelect = (mood) => {
+    setCurrentMood(mood);
+  };
+
+  useEffect(() => {
+    if (currentMood) {
+      //using find (returns object) instead of filter (returns array)
+      const moodData = moodRef.find((mood) => mood.moodName === currentMood);
+      setPostCurrentMood(moodData);
+    }
+  }, [currentMood]);
+
+  // ------- handle submit -------
+
+  const handlePreserve = () => {
+    if (postCurrentMood) {
+      console.log(postCurrentMood);
+      setCurrentMood("");
+      setPostCurrentMood("");
+    }
+  };
   return (
     <>
       <section className="mood__moodContainer">
-        <p className="mood__moodHeader">How's you mood right now?</p>
+        <p className="mood__moodHeader">Rate your mood today!</p>
         <div className="mood__moodSelector">
-          <div className="mood__joyful">
+          <div
+            onClick={() => handleMoodSelect("joyful")}
+            className="mood__joyful"
+          >
             😊
             <span className="mood__joyfulText">joyful</span>
           </div>
-          <div className="mood__content">
+
+          <div
+            onClick={() => handleMoodSelect("content")}
+            className="mood__content"
+          >
             🙂
             <span className="mood__contentText">content</span>
           </div>
-          <div className="mood__confused">
+          <div
+            onClick={() => handleMoodSelect("confused")}
+            className="mood__confused"
+          >
             😕
             <span className="mood__confusedText">confused</span>
           </div>
-          <div className="mood__angry">
+
+          <div
+            onClick={() => handleMoodSelect("angry")}
+            className="mood__angry"
+          >
             😠
             <span className="mood__angryText">angry</span>
           </div>
-          <div className="mood__crying">
+          <div
+            onClick={() => handleMoodSelect("crying")}
+            className="mood__crying"
+          >
             😢
             <span className="mood__cryingText">crying</span>
           </div>
         </div>
 
         <div className="mood__buttonContainer">
-          <button className="mood__preserve">preserve!</button>
+          <button
+            onClick={() => {
+              handlePreserve();
+            }}
+            className="mood__preserve"
+          >
+            preserve!
+          </button>
           <button className="mood__cancel">cancel</button>
         </div>
       </section>
