1ab99f122707b9f9fa44c7a886adbf0e9decbde4
Majedur Rahman <md.majedur97@gmail.com>
2024-10-22 14:32:56 -0230

    joyfulLineChart-base-implemented

--- src/Components/MoodAnalytics/MoodAnalytics.jsx
+++ src/Components/MoodAnalytics/MoodAnalytics.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./MoodAnalytics.scss";
 import PieChart from "./PieChart";
+import JoyfulChart from "./JoyfulChart";
 
 function MoodAnalytics({ moodMap, isMoodUpdated }) {
   const [moods, setMoods] = useState();
   const [moodLoading, setMoodLoading] = useState(true);
+  const [chartSelector, setChartSelector] = useState("pie");
   const url = import.meta.env.VITE_URL;
 
   useEffect(() => {
@@ -30,14 +32,63 @@ function MoodAnalytics({ moodMap, isMoodUpdated }) {
     emoji: moodMap[mood.moodName],
   }));
 
+  const chartHandler = (chart) => {
+    setChartSelector(chart);
+  };
   return (
     <main className="analytics">
       <div className="analytics__titleContainer">
         <h2 className="analytics__title">Mood Analytics </h2>
       </div>
-      <div className="analytics__chartContainer">
-        <PieChart mood={moodWithEmoji} moodMap={moodMap} />
+      <div className="analytics__buttonContainer">
+        <button
+          onClick={() => chartHandler("pie")}
+          className="analytics__content"
+        >
+          🥧
+        </button>
+        <button
+          onClick={() => chartHandler("joyful")}
+          className="analytics__content"
+        >
+          😊
+        </button>
+
+        <button
+          onClick={() => chartHandler("content")}
+          className="analytics__content"
+        >
+          🙂
+        </button>
+
+        <button
+          onClick={() => chartHandler("confused")}
+          className="analytics__content"
+        >
+          {" "}
+          😕{" "}
+        </button>
+
+        <button
+          onClick={() => chartHandler("angry")}
+          className="analytics__content"
+        >
+          😠
+        </button>
+
+        <button
+          onClick={() => chartHandler("crying")}
+          className="analytics__content"
+        >
+          😢
+        </button>
       </div>
+
+      {chartSelector === "pie" ? (
+        <PieChart mood={moodWithEmoji} moodMap={moodMap} />
+      ) : chartSelector === "joyful" ? (
+        <JoyfulChart mood={moodWithEmoji} />
+      ) : null}
     </main>
   );
 }
