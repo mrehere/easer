73d6901eca27cebd8b23c4608879a2ba42d7fdf1
Majedur Rahman <md.majedur97@gmail.com>
2024-10-23 12:06:14 -0230

    logo-addedOn-homePage

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import "./Header.scss";
+import logo from "../../../public/logo.png";
 function Header({ title, subtitle }) {
   return (
     <>
       <header className="header">
-        <h1 className="header__title">{title}</h1>
+        <div className="header__container">
+          <img className="header__logo" src={logo} alt="logo" />
+          <h1 className="header__title">{title}</h1>
+        </div>
         <h4 className="header__subTitle">{subtitle}</h4>
       </header>
     </>
