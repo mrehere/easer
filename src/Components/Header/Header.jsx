7567d53469c528b26c6453f8f96eacb4489b425d
Majedur Rahman <md.majedur97@gmail.com>
2024-10-23 12:13:00 -0230

    logo-click-functionality-added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,22 @@
 import "./Header.scss";
 import logo from "../../../public/logo.png";
+import { useNavigate } from "react-router-dom";
 function Header({ title, subtitle }) {
+  const navigate = useNavigate();
+
+  const handleLogo = () => {
+    navigate("/");
+  };
   return (
     <>
       <header className="header">
         <div className="header__container">
-          <img className="header__logo" src={logo} alt="logo" />
+          <img
+            onClick={() => handleLogo()}
+            className="header__logo"
+            src={logo}
+            alt="logo"
+          />
           <h1 className="header__title">{title}</h1>
         </div>
         <h4 className="header__subTitle">{subtitle}</h4>
