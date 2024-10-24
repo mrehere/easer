47413b8749c452cca14a2826b3abf9b75b158cb5
Majedur Rahman <md.majedur97@gmail.com>
2024-10-24 14:44:15 -0230

    env-firebaseKeys-updated

--- src/Components/auth/firebase.js
+++ src/Components/auth/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2dkY2pqPj0C24frxPuZx0QcQXp6eqU4w",
-  authDomain: "fir-d45f0.firebaseapp.com",
-  projectId: "fir-d45f0",
-  storageBucket: "fir-d45f0.appspot.com",
-  messagingSenderId: "109825849896",
-  appId: "1:109825849896:web:72964ae87bd38e113b531d",
-  measurementId: "G-JDXWDQRYWZ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
